de8cb88fd04a3316443afda42c88b3166457f6ed
dingdong <1428837023@qq.com>
2023-06-29 23:15:03 +0800

    init

---
+++ src/components/AddBlog.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="add-blog">
+        <h2>添加博客</h2>
+        <form>
+            <label>博客标题</label>
+            <input type="text" v-model="blog.title" required/>
+            <label>博客内容</label>
+            <textarea v-model="blog.content"></textarea>
+
+            <div id="checkboxes">
+                <label>Vue.js</label>
+                <input type="checkbox" value="Vue.js" v-model="blog.categories">
+                <label>Node.js</label>
+                <input type="checkbox" value="Node.js" v-model="blog.categories">
+                <label>React.js</label>
+                <input type="checkbox" value="React.js" v-model="blog.categories">
+                <label>Angular4</label>
+                <input type="checkbox" value="Angular4" v-model="blog.categories">
+            </div>
+
+            <label>作者：</label>
+            <select v-model="blog.author">
+                <option v-for="author in authors">
+                    {{ author }}
+                </option>
+            </select>
+
+            <button v-on:click.prevent="post">添加博客</button>
+
+        </form>
+
+        <hr/>
+
+        <div id="preview">
+            <h3>博客总览</h3>
+            <p>博客标题：{{ blog.title }}</p>
+            <p>博客内容：</p>
+            <p>{{blog.content}}</p>
+            <p>博客分类：</p>
+            <ul>
+                <li v-for="category in blog.categories">{{ category }}</li>
+            </ul>
+            <p>作者：{{ blog.author }}</p>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'add-blog',
+    data () {
+      return {
+        blog:{
+            title:"",
+            content:"",
+            categories:[],
+            author: ""
+        },
+        authors:[
+            "zhangsan",
+            "lisi",
+            "wangwu"
+        ]
+      }
+    },
+    methods:{
+        post:function(){
+            
+        }
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+
+  </style>
+  
